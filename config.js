3758190c32bbfd822ecd83e754be79dbb81b9e6a
bbombelka <b.bombelka@gmail.com>
2020-08-08 15:52:08 +0200

    Remove redis files

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   logStorageTimeInDays: 10,
-  cacheIsDisabled: false,
+  cacheIsDisabled: true,
   cacheExpiration: 10, // seconds
   accessTokenExpirationTime: 15 * 60, // seconds
   refreshTokenExpirationTime: '24h',
