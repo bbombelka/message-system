cf7512b62fc431c18e09f11454c58680e58c2ecc
Barty-Boy <b.bombelka@gmail.com>
2020-09-08 15:52:24 +0200

    Redis cleanup

--- services/redis.js
+++ services/redis.js
@@ -1,8 +1,20 @@
 const redis = require('redis');
 const REDIS_PORT = process.env.REDIS_PORT || 6379;
+const RedisServer = require('redis-server');
+const { resolve } = require('path');
 
-const redisClient = redis.createClient(REDIS_PORT, null, { max_attempts: 5, retry_max_delay: 100 });
+const server = new RedisServer({
+  port: REDIS_PORT,
+  bin: resolve('./redis/redis-server'),
+});
+
+server.open(() => {});
+
+const redisClient = redis.createClient(REDIS_PORT, null, {
+  max_attempts: 5,
+  retry_max_delay: 100,
+});
+
 redisClient.on('connect', () => console.log('redis connected'));
-redisClient.on('error', error => console.log(error));
 
 module.exports = redisClient;
